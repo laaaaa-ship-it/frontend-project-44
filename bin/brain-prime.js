30f942d6e320cbdefcaf1aa25f64905f7fffa1c3
Your Name <you@example.com>
2025-05-17 19:50:08 +0300

    9 шаг

---
+++ bin/brain-prime.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+
+const isPrime = (num) => {
+  if (num < 2) return false;
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
+    if (num % i === 0) return false;
+  }
+  return true;
+};
+
+const playPrimeGame = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+
+  const roundsCount = 3;
+
+  for (let i = 0; i < roundsCount; i += 1) {
+    const number = Math.floor(Math.random() * 100) + 1;
+    const correctAnswer = isPrime(number) ? 'yes' : 'no';
+
+    console.log(`Question: ${number}`);
+    const userAnswer = readlineSync.question('Your answer: ').toLowerCase();
+
+    if (userAnswer !== correctAnswer) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
+    }
+
+    console.log('Correct!');
+  }
+
+  console.log(`Congratulations, ${userName}!`);
+};
+
+playPrimeGame();
