42a41aad705a47c3eca8443333c0c8b71ed2eb8f
Your Name <you@example.com>
2025-05-18 15:55:50 +0300

    добавление ссылок в README

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/laaaaa-ship-it/frontend-project-44/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/laaaaa-ship-it/frontend-project-44/actions)
+[![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=laaaaa-ship-it_frontend-project-44&metric=alert_status)](https://sonarcloud.io/summary/new_code?id=laaaaa-ship-it_frontend-project-44)
--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,39 +1,39 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
+import readlineSync from 'readline-sync'
 
 const isPrime = (num) => {
-  if (num < 2) return false;
+  if (num < 2) return false
   for (let i = 2; i <= Math.sqrt(num); i += 1) {
-    if (num % i === 0) return false;
+    if (num % i === 0) return false
   }
-  return true;
-};
+  return true
+}
 
 const playPrimeGame = () => {
-  console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
-  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+  console.log('Welcome to the Brain Games!')
+  const userName = readlineSync.question('May I have your name? ')
+  console.log(`Hello, ${userName}!`)
+  console.log('Answer "yes" if given number is prime. Otherwise answer "no".')
 
-  const roundsCount = 3;
+  const roundsCount = 3
 
   for (let i = 0; i < roundsCount; i += 1) {
-    const number = Math.floor(Math.random() * 100) + 1;
-    const correctAnswer = isPrime(number) ? 'yes' : 'no';
+    const number = Math.floor(Math.random() * 100) + 1
+    const correctAnswer = isPrime(number) ? 'yes' : 'no'
 
-    console.log(`Question: ${number}`);
-    const userAnswer = readlineSync.question('Your answer: ').toLowerCase();
+    console.log(`Question: ${number}`)
+    const userAnswer = readlineSync.question('Your answer: ').toLowerCase()
 
     if (userAnswer !== correctAnswer) {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      console.log(`Let's try again, ${userName}!`);
-      return;
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`)
+      console.log(`Let's try again, ${userName}!`)
+      return
     }
 
-    console.log('Correct!');
+    console.log('Correct!')
   }
 
-  console.log(`Congratulations, ${userName}!`);
-};
+  console.log(`Congratulations, ${userName}!`)
+}
 
-playPrimeGame();
+playPrimeGame()
